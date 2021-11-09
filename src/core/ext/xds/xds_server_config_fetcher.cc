1949a48e50a81b405fb1f1d50cec49eb6bd85e98
yashykt <yashykt@users.noreply.github.com>
2021-11-09 04:23:39 +0000

    Automated change: Fix sanity tests

--- src/core/ext/xds/xds_server_config_fetcher.cc
+++ src/core/ext/xds/xds_server_config_fetcher.cc
@@ -274,7 +274,8 @@ class XdsServerConfigFetcher::FilterChainMatchManager::XdsServerConfigSelector
 class XdsServerConfigFetcher::FilterChainMatchManager::XdsServerConfigSelector::
     VirtualHostListIterator : public XdsRouting::VirtualHostListIterator {
  public:
-  VirtualHostListIterator(const std::vector<VirtualHost>& virtual_hosts)
+  explicit VirtualHostListIterator(
+      const std::vector<VirtualHost>& virtual_hosts)
       : virtual_hosts_(virtual_hosts) {}
 
   size_t Size() const override { return virtual_hosts_.size(); }
@@ -291,7 +292,8 @@ class XdsServerConfigFetcher::FilterChainMatchManager::XdsServerConfigSelector::
 class XdsServerConfigFetcher::FilterChainMatchManager::XdsServerConfigSelector::
     VirtualHost::RouteListIterator : public XdsRouting::RouteListIterator {
  public:
-  RouteListIterator(const std::vector<Route>& routes) : routes_(routes) {}
+  explicit RouteListIterator(const std::vector<Route>& routes)
+      : routes_(routes) {}
 
   size_t Size() const override { return routes_.size(); }
 
@@ -419,8 +421,7 @@ class XdsServerConfigFetcher::FilterChainMatchManager::
 XdsServerConfigFetcher::XdsServerConfigFetcher(
     RefCountedPtr<XdsClient> xds_client,
     grpc_server_xds_status_notifier notifier)
-    : xds_client_(std::move(xds_client)),
-      serving_status_notifier_(std::move(notifier)) {
+    : xds_client_(std::move(xds_client)), serving_status_notifier_(notifier) {
   GPR_ASSERT(xds_client_ != nullptr);
 }
 
@@ -468,7 +469,7 @@ XdsServerConfigFetcher::ListenerWatcher::ListenerWatcher(
     std::string listening_address)
     : xds_client_(std::move(xds_client)),
       server_config_watcher_(std::move(server_config_watcher)),
-      serving_status_notifier_(std::move(serving_status_notifier)),
+      serving_status_notifier_(serving_status_notifier),
       listening_address_(std::move(listening_address)) {}
 
 void XdsServerConfigFetcher::ListenerWatcher::OnListenerChanged(
