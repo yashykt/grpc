9bc3784d3414dec82d170097de59898f23fab077
yashykt <yashykt@users.noreply.github.com>
2021-11-17 19:00:00 +0000

    Automated change: Fix sanity tests

--- src/core/ext/xds/xds_server_config_fetcher.cc
+++ src/core/ext/xds/xds_server_config_fetcher.cc
@@ -336,7 +336,8 @@ class XdsServerConfigFetcher::ListenerWatcher::FilterChainMatchManager::
 
   class VirtualHostListIterator : public XdsRouting::VirtualHostListIterator {
    public:
-    VirtualHostListIterator(const std::vector<VirtualHost>* virtual_hosts)
+    explicit VirtualHostListIterator(
+        const std::vector<VirtualHost>* virtual_hosts)
         : virtual_hosts_(virtual_hosts) {}
 
     size_t Size() const override { return virtual_hosts_->size(); }
@@ -477,8 +478,7 @@ class XdsServerConfigFetcher::ListenerWatcher::FilterChainMatchManager::
 XdsServerConfigFetcher::XdsServerConfigFetcher(
     RefCountedPtr<XdsClient> xds_client,
     grpc_server_xds_status_notifier notifier)
-    : xds_client_(std::move(xds_client)),
-      serving_status_notifier_(std::move(notifier)) {
+    : xds_client_(std::move(xds_client)), serving_status_notifier_(notifier) {
   GPR_ASSERT(xds_client_ != nullptr);
 }
 
@@ -526,7 +526,7 @@ XdsServerConfigFetcher::ListenerWatcher::ListenerWatcher(
     std::string listening_address)
     : xds_client_(std::move(xds_client)),
       server_config_watcher_(std::move(server_config_watcher)),
-      serving_status_notifier_(std::move(serving_status_notifier)),
+      serving_status_notifier_(serving_status_notifier),
       listening_address_(std::move(listening_address)) {}
 
 void XdsServerConfigFetcher::ListenerWatcher::OnListenerChanged(
