33564162d93150f707e7f5689f4410c3ad079355
yashykt <yashykt@users.noreply.github.com>
2021-11-17 21:29:13 +0000

    Automated change: Fix sanity tests

--- src/core/ext/xds/xds_server_config_fetcher.cc
+++ src/core/ext/xds/xds_server_config_fetcher.cc
@@ -452,8 +452,7 @@ class XdsServerConfigFetcher::ListenerWatcher::FilterChainMatchManager::
 XdsServerConfigFetcher::XdsServerConfigFetcher(
     RefCountedPtr<XdsClient> xds_client,
     grpc_server_xds_status_notifier notifier)
-    : xds_client_(std::move(xds_client)),
-      serving_status_notifier_(std::move(notifier)) {
+    : xds_client_(std::move(xds_client)), serving_status_notifier_(notifier) {
   GPR_ASSERT(xds_client_ != nullptr);
 }
 
@@ -501,7 +500,7 @@ XdsServerConfigFetcher::ListenerWatcher::ListenerWatcher(
     std::string listening_address)
     : xds_client_(std::move(xds_client)),
       server_config_watcher_(std::move(server_config_watcher)),
-      serving_status_notifier_(std::move(serving_status_notifier)),
+      serving_status_notifier_(serving_status_notifier),
       listening_address_(std::move(listening_address)) {}
 
 void XdsServerConfigFetcher::ListenerWatcher::OnListenerChanged(
