e131175d47aadce85e6e8f15db47481591e8c947
yashykt <yashykt@users.noreply.github.com>
2022-02-26 11:08:26 +0000

    Automated change: Fix sanity tests

--- test/core/xds/certificate_provider_store_test.cc
+++ test/core/xds/certificate_provider_store_test.cc
@@ -49,7 +49,7 @@ class FakeCertificateProvider : public grpc_tls_certificate_provider {
  private:
   int cmp_impl(const grpc_tls_certificate_provider* other) const override {
     // TODO(yashykt): Maybe do something better here.
-    return grpc_core::QsortCompare(
+    return QsortCompare(
         static_cast<const grpc_tls_certificate_provider*>(this), other);
   }
 };
