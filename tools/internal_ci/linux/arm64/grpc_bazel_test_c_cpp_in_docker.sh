ee4fb10744a478f795f2fc4ac2cd73425d9e0c45
Yash Tibrewal <yashkt@google.com>
2024-08-22 22:34:05 +0000

    test

--- tools/internal_ci/linux/arm64/grpc_bazel_test_c_cpp_in_docker.sh
+++ tools/internal_ci/linux/arm64/grpc_bazel_test_c_cpp_in_docker.sh
@@ -25,5 +25,7 @@ bazel_test_c_cpp/bazel_wrapper \
   test --config=opt \
   --test_tag_filters=-no_linux,-no_arm64 \
   --build_tag_filters=-no_linux,-no_arm64 \
+  --runs_per_test=100
   -- \
-  //test/...
+  //test/core/end2end:retry_transparent_max_concurrent_streams_test
+
